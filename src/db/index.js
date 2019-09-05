0ee7d69262d70d7e9e4ea3dc44abf0b88af75670
ParhamZare <parhamzare1@gmail.com>
2019-09-05 17:58:26 +0430

    fix install error ver 0.0.3

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,15 @@
 import Dexie from 'dexie';
 import Config from '../Config'
 const db = new Dexie(Config.databaseName);
+//TODO add realm
+
 export const initDB = () => {
     return new Promise((resolve, reject) => {
         if (!db.isOpen()) {
             try {
                 db
                     .version(1)
-                    .stores({"redux-db": `id,data`});
+                    .stores({ "redux-db": `id,data` });
                 resolve(true)
             } catch (e) {
                 reject(e);
@@ -21,16 +23,16 @@ export const initDB = () => {
 export const write = (collectionName, data) => {
     console.log("DEXIE", collectionName, data)
     return new Promise((resolve, reject) => {
-        db.transaction("rw", db[collectionName], async() => {
-            if ((await db[collectionName].where({id: data.id}).count()) === 0) {
+        db.transaction("rw", db[collectionName], async () => {
+            if ((await db[collectionName].where({ id: data.id }).count()) === 0) {
                 db[collectionName].add(data);
                 console.log("stored data", data, collectionName)
                 resolve("stored")
             } else {
                 db[collectionName]
                     .update(data.id, {
-                    ...data
-                })
+                        ...data
+                    })
                     .then((response) => {
                         console.log(response);
                         resolve("updated")
